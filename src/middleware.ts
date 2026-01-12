77fcd42e8963df87bb09035b7a9314f2e4220989
Pranam-Sharma <pranamsharma.is420@gmail.com>
2026-01-12 18:42:27 +0530

    feat: implement basic auth for staging

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,33 @@
 
-import { type NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 import { updateSession } from '@/lib/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
+    // Basic Auth for Staging (or any environment where ENABLE_BASIC_AUTH is set)
+    if (process.env.ENABLE_BASIC_AUTH === 'true') {
+        const authBasic = request.headers.get('authorization')
+
+        if (authBasic) {
+            const [scheme, credentials] = authBasic.split(' ')
+            if (scheme === 'Basic' && credentials) {
+                const [user, pwd] = atob(credentials).split(':')
+                const desiredUser = process.env.BASIC_AUTH_USER || 'admin'
+                const desiredPwd = process.env.BASIC_AUTH_PASSWORD || 'password'
+
+                if (user === desiredUser && pwd === desiredPwd) {
+                    return await updateSession(request)
+                }
+            }
+        }
+
+        return new NextResponse('Auth Required', {
+            status: 401,
+            headers: {
+                'WWW-Authenticate': 'Basic realm="Secure Staging Area"',
+            },
+        })
+    }
+
     return await updateSession(request)
 }
 
